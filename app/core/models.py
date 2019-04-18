7341f3beab5390d499d4142e2b5a01fd514d5738
Yirogu <stachudcs@gmail.com>
2019-04-18 21:50:36 +0200

    pratice TDD

--- app/core/models.py
+++ app/core/models.py
@@ -15,9 +15,9 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self,email,password=None):
+    def create_superuser(self, email, password=None):
         """Creates and save a new super user """
-        user = self.create_user(email,password)
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -27,7 +27,6 @@ class ModelTests(TestCase):
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
 
-
     def test_create_new_super_user(self):
         """test creating a new super user """
         user = get_user_model().objects.create_superuser(
